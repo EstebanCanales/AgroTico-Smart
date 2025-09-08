617895fc33319c7bb8edc83d859d64c2e02123a0
stevenalp207 <s.alpizar207@gmail.com>
2025-09-08 21:54:20 +0100

    Base

--- main.py
+++ main.py
@@ -122,19 +122,19 @@ def control_remoto():
         if direccion == "izquierda":
             print("[CONTROL] IZQUIERDA")
             logger.info("Movimiento a la izquierda")
-            #stepper.move_left()
+            stepper.move_left()
         elif direccion == "derecha":
             print("[CONTROL] DERECHA")
             logger.info("Movimiento a la derecha")
-            #stepper.move_right()
+            stepper.move_right()
         elif direccion == "atras":
             print("[CONTROL] ATRÁS")
             logger.info("Movimiento hacia atrás")
-            #stepper.move_backward()
+            stepper.move_backward()
         elif direccion == "adelante":
             print("[CONTROL] ADELANTE")
             logger.info("Movimiento hacia adelante")
-            #stepper.move_forward()
+            stepper.move_forward()
         else:
             stepper.stop_motor()
 
